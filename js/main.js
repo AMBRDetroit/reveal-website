ee24a12794eaebe05eee43584f3c32936fa1f9a8
anthony <anthony@ambrdetroit.com>
2018-10-23 16:29:22 -0400

    allow URL param to load

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
-$(document).ready( function() {
-	const vid = document.getElementById("curtains"); 
+window.webapp.func : {
+	getUrlParameterByName : function(name) {
+		var url = window.location.search;
+		var name = name.replace(/[\[\]]/g, "\\$&");
+		var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+		var results = regex.exec(url);
+		if(!results || !results[2]) {
+			return "";
+		}
+		return decodeURIComponent(results[2].replace(/\+/g, " "));
+	}
+}
 
-	let website_url = prompt('Website URL to review:');
+$(document).ready( function() {
+	let website_url = webapp.func.getUrlParameterByName('url');
+	
+	if(website_url=='') {
+		website_url = prompt('Website URL to reveal:');
+	}
+	
+	if(website_url=='') {
+		window.location.reload();
+	}
+	
+	const vid = document.getElementById("curtains");  
 
 	$('.website').html('<iframe src="' + website_url + '"></iframe>');
 
